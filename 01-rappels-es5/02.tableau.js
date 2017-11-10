093e1dc24465106ac8cf43110ec5763b7e73d21e
Florent Callaou <florent.callaou22@gmail.com>
2017-11-10 12:10:39 +0100

    correctif lambda on ex2

--- 01-rappels-es5/02.tableau.js
+++ 01-rappels-es5/02.tableau.js
@@ -1,39 +1,16 @@
 var villes = ['nantes', 'paris', 'saint-nazaire', 'angers', 'le mans']
 
-villes.forEach(
-    function(ville){
-        console.log(ville)
-    }
-)
+villes.forEach(ville => console.log(ville))
 
-var lettreADansToutesLesVilles = villes.every(
-    function(ville){
-        return ville.includes('a')
-    }
-)
+var lettreADansToutesLesVilles = villes.every(ville => ville.includes('a'))
 console.log('lettreADansToutesLesVilles == ', lettreADansToutesLesVilles)
 
-var auMoinsUneVilleAvecUnTiret = villes.some(
-    function(ville){
-        return ville.includes('-')
-    }
-)
+var auMoinsUneVilleAvecUnTiret = villes.some(ville => ville.includes('-'))
 console.log('auMoinsUneVilleAvecUnTiret == ', auMoinsUneVilleAvecUnTiret)
 
-villesSansTiretSansEspace = villes.filter(
-    function(ville){
-        return (!ville.includes('-')) && (!ville.includes(' '))
-    }
-)
+villesSansTiretSansEspace = villes.filter(ville => (!ville.includes('-')) && (!ville.includes(' ')))
 console.log('villesSansTiretSansEspace == ', villesSansTiretSansEspace)
 
-villesMajusculeSeTerminantParS = villes.filter(
-    function(ville){
-        return (ville.slice(-1) == 's')
-    }
-).map(
-    function(ville){
-        return ville.toUpperCase()
-    }
-)
+villesMajusculeSeTerminantParS = villes.filter(ville => ville.slice(-1) == 's')
+                                        .map(ville => ville.toUpperCase())
 console.log('villesMajusculeSeTerminantParS == ', villesMajusculeSeTerminantParS)
